d513b98e30f48560cb2bc7479e01fb0a8bbc77cf
eddygld <61808648+eddygld@users.noreply.github.com>
2020-07-28 00:37:32 -0400

    src files
    For simple fun facts app

---
+++ app/src/main/java/com/example/funfacts/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.funfacts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    // Declare our view variables
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private FactBook factBook = new FactBook();
+    private ColorWheel colorWheel = new ColorWheel();
+    private TextView factTextView;
+    private Button showFactButton;
+    private RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assign the Views from the layout file to the corresponding variable
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+        relativeLayout = findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                   String fact = factBook.getFact();
+                   int color = colorWheel.getColor();
+
+                // Update the screen with our new fact
+                factTextView.setText(fact);
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+       // Toast.makeText(this, "Yay! Our activity was created!", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "We're logging from the onCreate() method");
+    }
+}
